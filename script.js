f479c89025e6b6112bc20a5c683dd7f0e92f4fc9
MdRiabi <mhdriabi2013@gmail.com>
2023-06-27 18:39:20 +0200

    array declarations

---
+++ script.js
@@ -0,0 +1,18 @@
+const numbers = [1,2,3,5];
+const moreNum = new Array(5); //array of 5 empty of length .
+const Tabb = Array.of(1,2);
+console.log("Tabb"); // create array containe 1 and 2.
+const vtab = Array.from('medium');// create array containe  'm' 'e' 'd' 'i' 'u' 'm' .
+console.log(vtab);
+
+
+const obje =  [30,'med', {vad:[1,2,5,]}]; // array of objects .
+
+const person = [[30,12,33],[15,2,23,56,4]];
+for(const data of person){
+    for(const da of data){
+        console.log(da);// for display array elements .
+    }
+}
+
+
