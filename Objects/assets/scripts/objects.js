bde080b664b6c9b8d781430c46afdad94a8ee5b1
MdRiabi <mhdriabi2013@gmail.com>
2023-06-29 18:29:12 +0200

    rendring the usre input in the list

--- Objects/assets/scripts/objects.js
+++ Objects/assets/scripts/objects.js
@@ -4,8 +4,30 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+//rendring the movie element in tthe list wich have the movie-list id.
+
+const renderMovies = () => {
+
+    const movieList = document.getElementById('movie-list');
+    if(movies.length === 0) {
+ movieList.classList.remove('visible');
+    }
+    else{
+        movieList.classList.add('visible');
+
+    }
+   
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        movieEl.textContent = movie.info.title;
+        movieList.append(movieEl);
+         movieList.innerHTML = '';
+    });
+};
+
+// add the movie in the movies list
 const addMovieHandler = () => {
-    const title = document.getElementById('li').value;
+    const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
     const extraValue = document.getElementById('extra-value').value;
     if(title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
@@ -22,7 +44,9 @@ const addMovieHandler = () => {
     };
 
     movies.push(newMovie);
+    renderMovies();
 };
 
 
 addMoviesBtn.addEventListener('click', addMovieHandler );
+
